d33a58552e6da0227d33174ef16eda7732597b49
MatthijsHulshof <matthijshulshof@live.nl>
2025-12-03 13:10:46 +0100

    stress test fix

--- tests/k6/stress-test.js
+++ tests/k6/stress-test.js
@@ -3,20 +3,24 @@ import { sleep, check } from 'k6';
 
 export let options = {
   stages: [
-    { duration: '10s', target: 10 }, // building to 10 users
-    { duration: '20s', target: 20 }, // then to 20 users
-    { duration: '10s', target: 0 },  // phase out to 0 users
+    { duration: '10s', target: 10 },
+    { duration: '20s', target: 20 },
+    { duration: '10s', target: 0 },
   ],
   thresholds: {
-    http_req_failed: ['rate<0.05'],   // less than 5% errors
-    http_req_duration: ['p(95)<800'], // 95% of the requests < 800ms
+    http_req_failed: ['rate<0.05'],
+    http_req_duration: ['p(95)<800'],
   },
 };
 
 export default function () {
   const baseUrl = __ENV.API_URL;
 
-  const res = http.get(`${baseUrl}/api/Names`);
+  const res = http.get(`${baseUrl}/api/Names`, {
+    headers: {
+      "Accept": "text/plain"
+    }
+  });
 
   check(res, {
     'status is 200': (r) => r.status === 200,
